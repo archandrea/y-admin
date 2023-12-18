259b7a21113b83af2455694eb5695ddd9e1079a8
xz <luc_xz@foxmail.com>
2023-12-18 17:47:17 +0800

    Revert "update: 打包配置优化"
    This reverts commit 98df96cd161fffb75db8b3bf11508b824d1c443e.

--- vue.config.js
+++ vue.config.js
@@ -38,50 +38,9 @@ module.exports = defineConfig({
       fallback: {
         path: require.resolve("path-browserify")
       },
-    },
-    plugins: [
-      new HappyPack({
-        id: 'happybabel',
-        loaders: ['babel-loader'],
-        threadPool: happyThreadPool
-      })
-    ]
+    }
   },
   chainWebpack(config) {
-    if (process.env.ENV === 'production') {
-      // 去除debugger, console
-      config.optimization.minimizer('terser').tap(args => {
-        args.forEach(item => {
-          if (item.hasOwnProperty('terserOptions')) {
-            Object.assign(item['terserOptions'].compress, {
-              drop_debugger: true,
-              drop_console: true,
-              pure_funcs: ['console.log']
-            })
-          }
-          item['terserOptions']['format'] = {
-            comments: false
-          }
-        })
-        return args
-      })
-    }
-
-    config
-      .plugin('preload')
-      .use('@vue/preload-webpack-plugin')
-      .tap(() => [
-        {
-          rel: 'preload',
-          // to ignore runtime.js
-          fileBlacklist: [/\.map$/, /hot-update\.js$/, /runtime\..*\.js$/],
-          include: 'initial'
-        }
-      ])
-    // 删除prefetch
-    config.plugins.delete('prefetch')
-
-    // svg
     config.module
       .rule('svg')
       .exclude.add(resolve('src/icons'))
@@ -97,38 +56,5 @@ module.exports = defineConfig({
         symbolId: 'icon-[name]'
       })
       .end()
-
-    // 拆分chunks
-    config
-      .optimization.splitChunks({
-        chunks: 'all',
-        cacheGroups: {
-          libs: {
-            name: 'chunk-libs',
-            test: /[\\/]node_modules[\\/]/,
-            priority: 10,
-            chunks: 'initial' // only package third parties that are initially dependent
-          },
-          elementUI: {
-            name: 'chunk-elementUI', // split elementUI into a single package
-            priority: 20, // the weight needs to be larger than libs and app or it will be packaged into libs or app
-            test: /[\\/]node_modules[\\/]_?element-ui(.*)/ // in order to adapt to cnpm
-          },
-          echarts: {
-            name: 'chunk-echarts',
-            priority: 20,
-            test: /[\\/]node_modules[\\/]_?echarts(.*)/
-          },
-          commons: {
-            name: 'chunk-commons',
-            test: resolve('src/components'), // can customize your rules
-            minChunks: 3, //  minimum common number
-            priority: 5,
-            reuseExistingChunk: true
-          }
-        }
-      })
-    // https:// webpack.js.org/configuration/optimization/#optimizationruntimechunk
-    config.optimization.runtimeChunk('single')
   }
 })
