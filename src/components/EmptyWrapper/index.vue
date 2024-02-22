380f57297e86f0fd6ef74524a936ce509b66385d
Andre <archandreaaaaaaa@gmail.com>
2024-02-22 13:50:47 +0800

    feat: 无数据占位组件封装

---
+++ src/components/EmptyWrapper/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="empty-wrapper y-container--tight no-padding">
+    <el-empty
+      v-if="toggle"
+      v-bind="attrs"></el-empty>
+    <slot v-else></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'EmptyWrapper',
+  components: {},
+  props: {
+    // 开关
+    toggle: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  data() {
+    return {}
+  },
+  computed: {
+    attrs() {
+      return {
+        // 默认值
+        image: require('@/assets/images/no-info.png'),
+        description: '暂无信息',
+        ...this.$attrs,
+      }
+    }
+  },
+  watch: {},
+  created() {},
+  mounted() {},
+  methods: {},
+}
+</script>
+
+<style lang="scss" scoped>
+.empty-wrapper {
+}
+</style>
