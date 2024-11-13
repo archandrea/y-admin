3cdbf85a8ce6626dab23643f1ed1a48c459823d8
xz <luc_xz@foxmail.com>
2024-11-13 10:46:29 +0800

    update: login入口文件

---
+++ src/views/login/index.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import App from './index.vue'
+import '@/plugins/element'
+import '@/http/axios.js'
+import '@/assets/style/common/index.scss'
+
+// icons
+import SvgIcon from './components/SvgIcon'
+Vue.component('SvgIcon', SvgIcon)
+const iconModules = require.context('./assets/icons', false, /\.svg$/)
+iconModules.keys().map(iconModules)
+
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App)
+}).$mount('#app')
